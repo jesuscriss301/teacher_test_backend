81aa64a936dcf31c5a744e06dc09a415c076a278
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-26 18:20:02 -0500

    create teacher test project

---
+++ src/test/java/com/teacher/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacher.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
